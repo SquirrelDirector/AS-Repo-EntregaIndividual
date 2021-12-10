a72afd79b6990ea35a68aeaf39e63cc188b7ea7d
Chris <56068195+SquirrelDirector@users.noreply.github.com>
2021-12-10 13:19:09 +0000

    Retoques en ficheros Vagrant

--- vagrant/script_node.sh
+++ vagrant/script_node.sh
@@ -5,8 +5,10 @@ sed -i '$ a 192.168.1.2 postgre-db' /etc/hosts
 echo "Moviendo fichero package.json..."
 cp /ficheros_node/package.json /home/vagrant/package.json
 cp /ficheros_node/package-lock.json /home/vagrant/package-lock.json
+cp /ficheros_node/server.js /home/vagrant/server.js
 chown vagrant:vagrant package.json
 chown vagrant:vagrant package-lock.json
+chown vagrant:vagrant server.js
 
 echo "Instalando plugins..."
 npm install pg
